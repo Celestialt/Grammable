9aad0e254cc3be46530349c18dc4d26a855be6fc
Kirstan Fitzptrick <you@example.com>
2017-05-04 15:55:44 +0000

    Generated comments model; edited comments migration file; generated comments controller; added specs (tests) to spec/controllers/comments_controller_spec.rb file; hooked up the routes file with comments (config/routes.rb file); added create action to app/controllers/comments_controller.rb file; added comment relationships to the gram model, comment model; and user model; (app/models/gram.rb; app/models/comment.rb; app/models/user.rb files); added user authentication to app/controllers/comments_controller.rb file; refactored code

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503152211) do
+ActiveRecord::Schema.define(version: 20170504024726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "message"
+    t.integer  "user_id"
+    t.integer  "gram_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "grams", force: :cascade do |t|
     t.text     "message"
